a23917ce0907c64e4c7448d416877d915895ad6e
Linus Törngren <linus@etnolit.se>
2016-09-01 14:05:28 +0200

    Added parsons programming-problems.

---
+++ exercises/parsons.py
@@ -0,0 +1,60 @@
+"""Parsons programming-problem
+
+Instruktion
+===========
+
+All kod som behövs för att lösa uppgiften finns. Raderna behöver byta plats
+och indenteras för att få fungerande kod som löser problemet.
+
+Alla docstrings har placerats före kodraderna som ska användas för problemet.
+
+"""
+
+
+"""Givet en lista med heltal, hitta det största och returnera."""
+max_n = n
+return max_n
+for n in numbers:
+max_n = numbers[0]
+if n > max_n:
+def find_max(numbers):
+
+
+"""Givet en lista, returnera alla värden på udda positioner."""
+result.append(item)
+for item in l:
+if index % 2 == 1:
+return result
+result = []
+def odd_list(l):
+index = 0
+index += 1
+
+
+"""Givet en lista med tal, beräkna den löpande summan."""
+return total
+total.append(current)
+for n in numbers:
+total = []
+def running_total(numbers):
+current = 0
+current += n
+
+
+"""Givet två sorterade listor, slå samman dess till en sorterad lista."""
+result.append(next_value)
+next_value = l1.pop(0)
+result.append(next_value)
+if len(l1) == 0:
+if len(l2) == 0:
+result.extend(l1)
+return result
+if len(l1) > 0 and len(l2) > 0:
+next_value = l2.pop(0)
+else:
+result.extend(l2)
+while len(l1) > 0 or len(l2) > 0:
+result = []
+def merge_sorted_lists(l1, l2):
+if l1[0] > l2[0]:
+return result
---
+++ tests/test_parsons.py
@@ -0,0 +1,37 @@
+import pytest
+
+
+@pytest.mark.skip('Not implemented yet.')
+def test_find_max():
+    from exercises.parsons import find_max
+
+    assert find_max([1, 5, 2]) == 5
+    assert find_max([7, 2, 2]) == 7
+    assert find_max([-3, -2, -4]) == -2
+
+
+@pytest.mark.skip('Not implemented yet.')
+def test_odd_list():
+    from exercises.parsons import odd_list
+
+    assert odd_list([]) == []
+    assert odd_list([1]) == []
+    assert odd_list(['a', 'b']) == ['b']
+    assert odd_list([1, 2, 3, 4, 5, 6, 7]) == [2, 4, 6]
+
+
+@pytest.mark.skip('Not implemented yet.')
+def test_running_total():
+    from exercises.parsons import running_total
+
+    assert running_total([2, 4, 3]) == [2, 6, 9]
+    assert running_total([2, 4, -7, 2, -1, 4]) == [2, 6, -1, 1, 0, 4]
+
+
+@pytest.mark.skip('Not implemented yet.')
+def test_merge_sorted_lists():
+    from exercises.parsons import merge_sorted_lists
+
+    assert merge_sorted_lists([1, 3, 5, 7], [2, 4, 6]) == [1, 2, 3, 4, 5, 6, 7]
+    assert merge_sorted_lists([], [2, 4, 7]) == [2, 4, 7]
+    assert merge_sorted_lists([1, 1, 4, 7], [1, 3]) == [1, 1, 1, 3, 4, 7]
